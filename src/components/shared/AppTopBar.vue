d1fdd15cf3e8f436a5fd4a33a89a9f7e460f8468
Katherine Sousa Pedreira <katherinesousapedreira@MacBook-Pro-de-Katherine.local>
2022-03-18 18:51:37 -0300

    Nova feature para escolha do tema

--- src/components/shared/AppTopBar.vue
+++ src/components/shared/AppTopBar.vue
@@ -1,10 +1,28 @@
 <template>
     <div class="app-top-bar">
         <v-app-bar app color="green" dark>
-            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-                <v-toolbar-title>
-                    Campeonato Brasileiro ⚽
-                </v-toolbar-title>
+            <v-app-bar-nav-icon @click="drawer = true">
+            </v-app-bar-nav-icon> 
+            <v-toolbar-title class="mr-8">
+                Campeonato Brasileiro ⚽
+            </v-toolbar-title>
+            <v-tooltip v-if="!$vuetify.theme.dark" bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn v-on="on" v-bind="attrs" color="transparent" small fab @click="darkMode">
+                        <v-icon class="mr-1">mdi-moon-waxing-crescent</v-icon>
+                    </v-btn>
+                </template>
+                <span>Dark Mode Off</span>
+            </v-tooltip>
+
+            <v-tooltip v-else bottom>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn v-on="on" v-bind="attrs" color="transparent" small fab @click="darkMode">
+                        <v-icon>mdi-white-balance-sunny</v-icon>
+                    </v-btn>
+                </template>
+                <span>Dark Mode On</span>
+            </v-tooltip>
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" absolute temporary>
@@ -46,6 +64,11 @@ export default {
         drawer: false,
         group: null,
     }),
+    methods: {
+        darkMode() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        }
+    }
 }
 </script>
 
@@ -54,4 +77,5 @@ export default {
 .app-top-bar a {
     text-decoration: none;
 }
+
 </style>
