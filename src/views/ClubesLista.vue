c413d26d635f6c13f2dbd93163ee562bfac0ea1e
Katherine Sousa Pedreira <katherinesousapedreira@MacBook-Pro-de-Katherine.local>
2022-03-19 08:56:35 -0300

    arrumando a identacao

--- src/views/ClubesLista.vue
+++ src/views/ClubesLista.vue
@@ -3,37 +3,36 @@
         <h2 class="text-h5 text-center mb-3 mt-5">
             Classificação
         </h2>
-
-                <template>
-        <v-simple-table dark>
-            <template v-slot:default>
-            <thead>
-                <tr>
-                <th colspan="2" class="text-left">
-                    Clubes
-                </th>
-                <th class="text-right">
-                    Pontos
-                </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(clube, index) of clubesListaOrdenada" :key="clube.id">
-                    <td>{{ index + 1 }} </td>
-                        <td>
-                            <v-avatar size="24">
-                                <img 
-                                    :src="clube.escudo"
-                                    :alt="clube.nome"
-                                >
-                            </v-avatar>
-                            <span class="pl-2">{{ clube.nome }}</span>
-                        </td>          
-                        <td class="text-right">{{ clube.pontos }}</td>
-                </tr>
-            </tbody>
-            </template>
-        </v-simple-table>
+        <template>
+            <v-simple-table dark>
+                <template v-slot:default>
+                    <thead>
+                        <tr>
+                            <th colspan="2" class="text-left">
+                                Clubes
+                            </th>
+                            <th class="text-right">
+                                Pontos
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(clube, index) of clubesListaOrdenada" :key="clube.id">
+                            <td>{{ index + 1 }} </td>
+                                <td>
+                                    <v-avatar size="24">
+                                        <img 
+                                            :src="clube.escudo"
+                                            :alt="clube.nome"
+                                        >
+                                    </v-avatar>
+                                    <span class="pl-2">{{ clube.nome }}</span>
+                                </td>          
+                                <td class="text-right">{{ clube.pontos }}</td>
+                        </tr>
+                    </tbody>
+                </template>
+            </v-simple-table>
         </template>
     </v-container>
 </template>
